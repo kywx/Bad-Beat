ecceb9baea1dff2a395e0ab8239a9cfc43247c8b
Davidps255 <davidps@uci.edu>
2025-10-20 16:41:19 -0700

    Moving Platform
    Start

---
+++ Assets/Scripts/Platforms/MovingPlatform.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class MovingPlatformScript : MonoBehaviour
+{
+    public Transform targetA;
+    public Transform targetB;
+    public float speed;
+
+    private Transform curTarget;
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start() {
+        
+    }
+
+    // Update is called once per frame
+    void Update() {
+        
+    }
+}
