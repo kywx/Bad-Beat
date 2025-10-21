f061788edf8b96cc3b9a8c43b5daaeefb3d45e1d
Davidps255 <davidps@uci.edu>
2025-10-21 09:18:40 -0700

    Platform moves
    Started work on allowing rotation

--- Assets/Scripts/Platforms/MovingPlatform.cs
+++ Assets/Scripts/Platforms/MovingPlatform.cs
@@ -1,20 +1,35 @@
+using System;
+using System.Threading.Tasks;
 using UnityEngine;
 
 public class MovingPlatformScript : MonoBehaviour
 {
     public Transform targetA;
     public Transform targetB;
-    public float speed;
+    public float moveSpeed;
+    public float rotateSpeed;
+    public float delay;
 
     private Transform curTarget;
+    private bool delayed;
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start() {
-        
+        curTarget = targetA;
+        delayed = false;
     }
 
     // Update is called once per frame
-    void Update() {
-        
+    async Task Update() {
+        if (!delayed) {
+            transform.position = Vector3.MoveTowards(transform.position, curTarget.position, speed * Time.deltaTime);
+            transform.rotation = Vector3.RotateTowards(transform.rotation, curTarget.rotation, speed * Time.deltaTime);
+            if (Vector3.Distance(transform.position, curTarget.position) < 0.1f) {
+                delayed = true;
+                curTarget = (curTarget == targetA ? targetB : targetA);
+                await Task.Delay(TimeSpan.FromSeconds(delay));
+                delayed = false;
+            }
+        }
     }
 }
