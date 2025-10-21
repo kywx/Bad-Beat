2501cf3c7b4d63f78dfdbcf8def59e0887f2fede
Rocky Hankin <100500846+rhankin214@users.noreply.github.com>
2025-10-21 02:29:34 -0700

    player damage + respawn system and checkpoints (#22)
    * respawn + checkpoint system + a hazard to check with
    
    * updated a variable scope
    
    * renamed function
    
    * refactored health and respawn into separate components
    
    * renamed helpfuls folder
    
    * renamed folder again xdd
    
    * ok I think this name makes sense
    
    * stored player stats in a separate scripted object
    
    * set attack damage to a value
    
    * renamed PlayerHealth script now that its only in charge of health
    
    * removed pointless variable
    
    ---------
    
    Co-authored-by: Rocky Hankin <rocdoc@gmail.com>

---
+++ Assets/Scripts/Player/PlayerRespawn.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class PlayerRespawn : MonoBehaviour {
+    public Vector2 default_spawn_point; //default spawn point to be set for each scene.
+    private PlayerHealth HealthManager;
+
+    private Vector2 spawn_point;
+
+    public void UpdateCheckpoint(Vector2 new_pos){
+        spawn_point = new_pos;
+    }
+
+    public void Awake(){
+        HealthManager = this.GetComponent<PlayerHealth>();
+    }
+
+    public void Respawn(){
+        this.transform.position = spawn_point; 
+        if(HealthManager != null){
+            HealthManager.HealToMax();
+        }
+    }
+}
