556dddbdd293b425d870907c55f62265739e6071
Davidps255 <davidps@uci.edu>
2025-10-21 22:39:27 -0700

    Disappearing Platforms

--- Assets/Scripts/Platforms/DisappearingPlatform.cs
+++ Assets/Scripts/Platforms/DisappearingPlatform.cs
@@ -1,26 +1,41 @@
 using System;
 using System.Threading.Tasks;
 using UnityEngine;
+using static UnityEngine.GraphicsBuffer;
 
 public class DisappearingPlatform : MonoBehaviour
 {
-    public float delay = 1f;
-    public float respawnTime = 2f;
+    public float delay;
+    public float changeDuration;
+    public bool fadeEnabled;
 
-    private bool visible = true;
-    
+    private bool visible;
+    private Color curCol;
+
+    private void Start() {
+        visible = true;
+        curCol = this.GetComponent<SpriteRenderer>().color;
+    }
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     private void OnCollisionEnter2D(Collision2D collision) {
-        if (collision.gameObject.CompareTag("Player")) {
-            Task.Delay(TimeSpan.FromSeconds(delay));
-
-            Task.Delay(TimeSpan.FromSeconds(respawnTime));
-
+        if (fadeEnabled & visible & collision.gameObject.CompareTag("Player")) {
+            Invoke("fade", delay);
         }
     }
 
     private void fade() {
-        float timer = 0f;
-
+        if (visible) {
+            curCol.a = 0f; // Turns platform invisible
+            visible = false;
+            this.GetComponent<SpriteRenderer>().color = curCol;
+            this.GetComponent<Rigidbody2D>().simulated = false;
+            Invoke("fade", changeDuration);
+        } else {
+            curCol.a = 1f; // Turns platform visible
+            this.GetComponent<Rigidbody2D>().simulated = true;
+            this.GetComponent<SpriteRenderer>().color = curCol;
+            visible = true;
+        }
     }
 }
--- Assets/Scripts/Platforms/MovingPlatform.cs
+++ Assets/Scripts/Platforms/MovingPlatform.cs
@@ -4,26 +4,31 @@ using UnityEngine;
 
 public class MovingPlatformScript : MonoBehaviour
 {
-    public Transform targetA; // First target
-    public Transform targetB; // Second target
+    public GameObject targetA; // First target
+    public GameObject targetB; // Second target
+
     public float moveSpeed; // Units per second
     public float rotateSpeed; // Degrees per second
     public float delay; // Time in seconds it will wait once reatching a target before moving on
     public bool requireRotation; // Requires the platform finishes rotating before moving to next target
     public bool infRotate; // Determines if rotations should be done infinitely (For something like a saw blade))
     public bool rotateRight; // When indefinite, if true will rotate right, if false will rotate left
+    public bool waitForPlayer;
 
     private Transform curTarget; // Current target
     private bool delayed; // Determines if it should move or not
     
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start() {
-        curTarget = targetA;
+        curTarget = targetA.transform;
         delayed = false;
+        targetA.GetComponent<SpriteRenderer>().color = new Color(1f, 1f, 1f, 0f); // Turns target blocks invisible
+        targetB.GetComponent<SpriteRenderer>().color = new Color(1f, 1f, 1f, 0f);
     }
 
-    // Update is called once per frame
     async Task Update() {
+        if (waitForPlayer) {
+            return;
+        }
         if (infRotate) {
             if (rotateRight) {
                 transform.Rotate(new Vector3(0, 0, rotateSpeed) * Time.deltaTime * -1);
@@ -41,7 +46,7 @@ public class MovingPlatformScript : MonoBehaviour
                     return;
                 }
                 delayed = true;
-                curTarget = (curTarget == targetA ? targetB : targetA);
+                curTarget = (curTarget == targetA.transform ? targetB.transform : targetA.transform);
                 await Task.Delay(TimeSpan.FromSeconds(delay));
                 delayed = false;
             }
@@ -50,6 +55,7 @@ public class MovingPlatformScript : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D collision) {
         if (collision.gameObject.CompareTag("Player")) {
+            waitForPlayer = false;
             collision.transform.SetParent(this.transform);
         }
     }
