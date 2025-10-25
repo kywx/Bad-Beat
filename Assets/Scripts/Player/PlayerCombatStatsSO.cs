694228c4ae1fd80d971ca65028ef34a7e0ec3829
MaghyC <129014665+MaghyC@users.noreply.github.com>
2025-10-25 13:57:02 -0700

    Revert "update my branch, so that I can commit change on it without interupt main"

--- Assets/Scripts/Player/PlayerCombatStatsSO.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-[CreateAssetMenu(menuName = "Player Stats")]
-public class PlayerCombatStatsSO : ScriptableObject{
-
-    public int MaxHealth = 2;
-    public int AttackDamage = 1;
-    
-}
