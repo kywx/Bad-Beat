e84ca6edd83ed1b34383eeff1ad0aa2b1b8782c2
Rocky Hankin <rocdoc@gmail.com>
2025-10-31 14:50:25 -0700

    Poison and burning platforms

--- Assets/Scripts/Player/PlayerRespawn.cs
+++ Assets/Scripts/Player/PlayerRespawn.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 public class PlayerRespawn : MonoBehaviour {
     public Vector2 default_spawn_point; //default spawn point to be set for each scene.
     private PlayerHealth HealthManager;
+    private PlayerPoisoner PoisonManager;
 
     private Vector2 spawn_point;
 
@@ -12,12 +13,18 @@ public class PlayerRespawn : MonoBehaviour {
 
     public void Awake(){
         HealthManager = this.GetComponent<PlayerHealth>();
+        PoisonManager = this.GetComponent<PlayerPoisoner>();
     }
 
     public void Respawn(){
-        this.transform.position = spawn_point; 
-        if(HealthManager != null){
+        this.transform.position = spawn_point;
+        if (HealthManager != null)
+        {
             HealthManager.HealToMax();
+        }
+        if (PoisonManager != null)
+        {
+            PoisonManager.CurePoison();
         }
     }
 }
