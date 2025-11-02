1fb146460a3e0673f6dbc2a6dce55db22a40b32e
AdanChavez79 <adan7chavez9@gmail.com>
2025-11-01 18:19:07 -0700

    Enemy movement implemented + prefab

---
+++ Assets/Scripts/Enemy/Rat/RatMovement.cs
@@ -0,0 +1,73 @@
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class RatMovement : EnemyMovementTemplate
+{
+
+    [SerializeField] private Rigidbody2D rb;
+    [SerializeField] private Transform ray;
+    bool facingRight = true;
+    
+    protected override void RunPatrol()
+    {
+        bool hit = Physics2D.Raycast(ray.position, Vector2.down, 1f, LayerMask.GetMask("Ground"));
+        //Debug.DrawRay(ray.position, Vector2.down * 1f, Color.red);
+        if (hit == true) {
+            if (facingRight == true) {
+                //Debug.Log("Going right");
+                rb.linearVelocity = new Vector2(_groundSpeed, rb.linearVelocityY);
+            } else {
+                //Debug.Log("Going left");
+                rb.linearVelocity = new Vector2(-_groundSpeed, rb.linearVelocityY);
+
+            }
+        } else {
+            //Debug.Log("Turn");
+            facingRight = !facingRight;
+            transform.localScale = new Vector3(-transform.localScale.x, transform.localScale.y, transform.localScale.z);
+        }
+
+    }
+
+    protected override void RunIdle()
+    {
+        base.RunIdle();
+        rb.linearVelocity = new Vector2(0, rb.linearVelocityY);
+    }
+
+
+    protected override void DetermineMovement()
+    {
+        if (_grounded == true)
+        {
+            _movementType = EnemyMovement.Patrol;
+        }
+        else {
+            _movementType = EnemyMovement.Idle;
+
+        }
+
+
+    }
+    protected override void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.layer == LayerMask.NameToLayer("Ground"))
+        {
+            _grounded = true;
+            //Debug.Log("Grounded");
+
+        }
+    }
+
+    protected override void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.layer == LayerMask.NameToLayer("Ground"))
+        {
+            _grounded = false;
+            //Debug.Log("Falling");
+
+        }
+    }
+
+
+}
