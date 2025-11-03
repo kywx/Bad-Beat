a386442b04970a5289c5a98000d5bf6c8aa9cbff
Davidps255 <davidps@uci.edu>
2025-11-02 22:33:59 -0800

    Blackout + Fade in

--- Assets/Scripts/Player/PlayerRespawn.cs
+++ Assets/Scripts/Player/PlayerRespawn.cs
@@ -1,8 +1,12 @@
 using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
 
 public class PlayerRespawn : MonoBehaviour {
     public Vector2 default_spawn_point; //default spawn point to be set for each scene.
     private PlayerHealth HealthManager;
+    public Image BlackImage;
+    public float FadeDuration = 0.5f;
 
     private Vector2 spawn_point;
 
@@ -14,10 +18,25 @@ public class PlayerRespawn : MonoBehaviour {
         HealthManager = this.GetComponent<PlayerHealth>();
     }
 
-    public void Respawn(){
+    public void Respawn() {
+        BlackImage.color = Color.black;
         this.transform.position = spawn_point; 
         if(HealthManager != null){
             HealthManager.HealToMax();
+            StartCoroutine(FadeCoroutine());
+        }
+    }
+
+    private IEnumerator FadeCoroutine() {
+        print("Coroutine");
+        float timer = 0f;
+        Color curColor = BlackImage.color;
+
+        while (timer < FadeDuration) {
+            timer += Time.deltaTime;
+            float newAlpha = Mathf.Lerp(1, 0, timer / FadeDuration);
+            BlackImage.color = new Color(curColor.r, curColor.g, curColor.b, newAlpha);
+            yield return null;
         }
     }
 }
