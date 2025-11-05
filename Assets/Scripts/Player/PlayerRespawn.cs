742430c269070f210abf8d64769a1d6fa9f4494e
Davidps255 <davidps@uci.edu>
2025-11-04 21:23:00 -0800

    Blackout on death with a fade back in (#37)
    * Ground tags added to platforms
    
    * Blackout + Fade in
    
    * Edited Fade Duration
    
    * Move fade coroutine call

--- Assets/Scripts/Player/PlayerRespawn.cs
+++ Assets/Scripts/Player/PlayerRespawn.cs
@@ -1,8 +1,12 @@
 using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
 
 public class PlayerRespawn : MonoBehaviour {
     public Vector2 default_spawn_point; //default spawn point to be set for each scene.
     private PlayerHealth HealthManager;
+    public Image BlackImage;
+    public float FadeDuration = 0.5f;
     private PlayerPoisoner PoisonManager;
 
     private Vector2 spawn_point;
@@ -16,8 +20,9 @@ public class PlayerRespawn : MonoBehaviour {
         PoisonManager = this.GetComponent<PlayerPoisoner>();
     }
 
-    public void Respawn(){
+    public void Respawn() {
         this.transform.position = spawn_point;
+        BlackImage.color = Color.black;
         if (HealthManager != null)
         {
             HealthManager.HealToMax();
@@ -25,6 +30,20 @@ public class PlayerRespawn : MonoBehaviour {
         if (PoisonManager != null)
         {
             PoisonManager.CurePoison();
+        }
+        StartCoroutine(FadeCoroutine());
+    }
+
+    private IEnumerator FadeCoroutine() {
+        print("Coroutine");
+        float timer = 0f;
+        Color curColor = BlackImage.color;
+
+        while (timer < FadeDuration) {
+            timer += Time.deltaTime;
+            float newAlpha = Mathf.Lerp(1, 0, timer / FadeDuration);
+            BlackImage.color = new Color(curColor.r, curColor.g, curColor.b, newAlpha);
+            yield return null;
         }
     }
 }
