30379e5662e63502250939c7b2890d5eb9e7638e
Kylie <kyliehu000@gmail.com>
2025-11-16 15:05:19 -0800

    Dialogue System
    Dialogue Panel prefab - add to a Canvas object
    DialogueManager.cs - add to empty game object, connect to dialogue panel prefab items
    DialogueData.cs - add to an interactable game object

---
+++ Assets/Scripts/Dialogue/DialogueData.cs
@@ -0,0 +1,51 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueData : MonoBehaviour, IInteractable
+{
+    public bool CanInteract()
+    {
+        return true;
+    }
+
+    public void Interact()
+    {
+        if (!CanInteract())
+        {
+            return;
+        }
+
+        DialogueManager.Instance.StartDialogue(this);
+        Debug.Log("Finished?");
+    }
+
+    [SerializeField] private Sprite OtherSprite = null;
+
+    [SerializeField]
+    private int speakerCount = 1;
+
+    [System.Serializable]
+    public class DialogueLine
+    {
+        public string speakerName;
+        public string lineText;
+    }
+
+    [SerializeField]
+    private List<DialogueLine> speakerLines = new List<DialogueLine>();
+
+    public List<DialogueLine> getLines()
+    {
+        return speakerLines;
+    }
+
+    public int getSpeakerCount()
+    {
+        return speakerCount;
+    }
+
+    public Sprite getOtherSprite()
+    {
+        return OtherSprite;
+    }
+}
