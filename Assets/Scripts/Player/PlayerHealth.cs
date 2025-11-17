de7607a27ad71b4d20562992a48c177cb05a84c9
Rocky Hankin <rocdoc@gmail.com>
2025-11-16 21:19:03 -0800

    re-added iframes to PlayerHealth

--- Assets/Scripts/Player/PlayerHealth.cs
+++ Assets/Scripts/Player/PlayerHealth.cs
@@ -10,8 +10,6 @@ public class PlayerHealth : MonoBehaviour
 
     private float iframeTimer;
 
-    
-    private void Awake(){
     // Property to allow UI to read health without reflection
     public int CurrentHealth => _health;
     public int MaxHealth => stats != null ? stats.MaxHealth : 0;
@@ -33,14 +31,17 @@ public class PlayerHealth : MonoBehaviour
 
     public void Damage(int dmg)
     {
-        _health -= dmg;
+        if(iframeTimer > 0){
+            _health -= dmg;
 
         // Clamp to prevent negative health
-        _health = Mathf.Max(0, _health);
+            _health = Mathf.Max(0, _health);
+            iframeTimer = stats.iframes;
 
-        if (_health <= 0 && RespawnManager != null)
-        {
-            RespawnManager.Respawn();
+            if (_health <= 0 && RespawnManager != null)
+            {
+                RespawnManager.Respawn();
+            }
         }
     }
 
