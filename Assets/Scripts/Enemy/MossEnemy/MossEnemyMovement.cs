dc601e50c84dc7737d99744e82b1ee95d0ad5abd
Rocky Hankin <100500846+rhankin214@users.noreply.github.com>
2025-11-19 12:30:41 -0800

    moss guy (#50)
    * moss guy
    
    * re-added iframes to PlayerHealth
    
    ---------
    
    Co-authored-by: Rocky Hankin <rocdoc@gmail.com>

---
+++ Assets/Scripts/Enemy/MossEnemy/MossEnemyMovement.cs
@@ -0,0 +1,122 @@
+using System.Linq;
+using Unity.Mathematics;
+using UnityEngine;
+
+public class MossEnemyMovement: EnemyMovementTemplate
+{
+    
+    private MossEnemyAttack _attackManager;
+    private SpriteRenderer spr;
+
+    [SerializeField]
+    private Rigidbody2D rb;
+
+    [SerializeField]
+    private float sightWidth; //vertical range player can be in where moss guy will take the shot.
+
+    [SerializeField]
+    private float groundCheckDistance; //
+    [SerializeField]
+    private float shootDistance; //distance player can be in 
+
+    [SerializeField]
+    private bool fallOffPlatforms;
+    private bool _isFacingRight;
+    private float radius;
+    protected override void Awake()
+    {
+        if(_patrolPoints.Count == 0)
+            _patrolPoints = null;
+        
+        base.Awake();
+        radius = this.transform.localScale.x/2;
+        _isFacingRight = true;
+        _attackManager = this.GetComponent<MossEnemyAttack>();
+        spr = this.GetComponent<SpriteRenderer>();
+    }
+
+    protected override void DetermineMovement()
+    {
+        Vector3 playerPos = _player.transform.position;
+        
+        if(math.abs(playerPos.y - this.transform.position.y) < sightWidth
+        && math.abs(playerPos.x - this.transform.position.x) < shootDistance)
+        {
+            _movementType = EnemyMovement.Unique;
+        }
+
+        else if (math.abs(playerPos.x - this.transform.position.x) < _startChaseDistance)
+        {
+            _movementType = EnemyMovement.Chase;
+        }
+        
+        else
+        {
+            _movementType = EnemyMovement.Patrol;
+        }
+    }
+
+
+    protected override void RunPatrol()
+    {
+        //walk back and forth
+        avoidFalling();
+        rb.linearVelocityX = _groundSpeed;
+        
+        if (_grounded)
+        {
+            Debug.Log("attempting jump");
+            rb.linearVelocityY = 5;
+        }
+    }
+    protected override void RunChase()
+    {
+        if(!avoidFalling() && (_player.transform.position.x - this.transform.position.x) * _groundSpeed < 0)
+        {
+            turn();
+        }
+        rb.linearVelocityX = _groundSpeed;
+
+    }
+    protected override void RunUnique()
+    {
+        if(_grounded){
+            _attackManager.Shoot(_isFacingRight);
+            rb.linearVelocityX = 0;
+        }
+        if((_player.transform.position.x - this.transform.position.x) * _groundSpeed < 0)
+        {
+            turn();
+        }
+    }
+    protected override void OnCollisionEnter2D(Collision2D collision)
+    {
+        base.OnCollisionEnter2D(collision);
+        if(collision.gameObject.tag == "Wall")
+        {
+            turn();
+        }
+    }
+    private void turn()
+    {
+        _isFacingRight = !_isFacingRight;
+        _groundSpeed *= -1;
+        spr.flipX = !spr.flipX;
+        Debug.Log(_grounded);
+    }
+    private bool avoidFalling()
+    {
+        if(!fallOffPlatforms){
+            float offset = radius * (_isFacingRight ? 1 : -1) * 2;
+            
+            Vector2 startingPoint = new Vector2(this.transform.position.x + offset, this.transform.position.y);
+            bool aboveGround = Physics2D.Raycast(startingPoint, Vector2.down, groundCheckDistance, LayerMask.GetMask("Ground"));
+            if (!aboveGround)
+            {
+                turn(); //turn to avoid falling off a platform
+                return true;
+            }
+        }
+        return false;
+    }
+}
