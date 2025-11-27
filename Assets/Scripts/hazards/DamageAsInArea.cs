3b8671195b5d94da5b63c8b2d3ac00af5ea24305
Davidps255 <davidps@uci.edu>
2025-11-27 01:13:49 -0800

    Fixed Spikes
    Corrected spikes dealing damage multiple times

--- Assets/Scripts/hazards/DamageAsInArea.cs
+++ Assets/Scripts/hazards/DamageAsInArea.cs
@@ -1,11 +1,20 @@
+using System.Collections;
 using UnityEngine;
 
 public class DamageAsInArea : MonoBehaviour {
     public int damage_value;
+    private bool debounce = false;
+
+    IEnumerator Debounce(float delay) {
+        debounce = true;
+        yield return new WaitForSeconds(delay);
+        debounce = false;
+    }
 
     void OnCollisionStay2D(Collision2D collision) {
         PlayerHealth health = collision.gameObject.GetComponent<PlayerHealth>();
-        if (health != null) {
+        if (health != null && !debounce) {
+            StartCoroutine(Debounce(0.5f));
             health.Damage(damage_value);
         }
     }
